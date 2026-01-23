ff69ae033af19d218cb960bf79330c7976414970
hari1907-code <hariguna2561@gmail.com>
2026-01-23 19:41:27 +0530

    organised file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>ghjd</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      
+    </Routes>
+    </BrowserRouter>
+
    </StrictMode>,
 )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+
+const HomePage  = (props) =>{
+
+    return(
+        <>
+        <h1>hbjv</h1>
+        {/* TODO:NEED BUTTONS */}
+        </>
+    )
+}
+
+
+export default HomePage
