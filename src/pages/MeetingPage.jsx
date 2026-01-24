ae4c95a4af9180b9bc173a4159fe8562915bca90
hari1907-code <hariguna2561@gmail.com>
2026-01-24 12:57:07 +0530

    added video and camer button

--- src/pages/MeetingPage.jsx
+++ src/pages/MeetingPage.jsx
@@ -1,4 +1,4 @@
-import { Mic } from "lucide-react"
+import { Mic,Video,Camera } from "lucide-react"
 import Meet from "../components/MeetingNavbut"   
 import ChatBox from "../components/ChatBox"
 
@@ -6,18 +6,21 @@ const MeetingPage  = () =>{
 
     return(
         <>
+        <h1>Meeting Page</h1>
         <div className="navbar bg-base-300">
             <div className="navbar-center">
-                <Meet inField = {<Camera />} color = "neutral-content"/>
+                <Meet inField = {<Video />} color = "neutral-content"/>
                 <Meet inField = {<Mic />} color = "neutral-content"/>
                 <Meet inField = {<Camera />} color = "neutral-content"/>
             </div>
-            <div></div> /* Video Box */
-            <div>
-                <ChatBox />
-            </div>
-            
         </div>
+        <div className="card card-side bg-base-100 shadow-sm">
+            
+        </div> /* Video Box */
+        <div>
+            <ChatBox />
+        </div>
+            
         </>
     )
 }
