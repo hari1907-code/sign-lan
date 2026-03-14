8fe3b9859f81bacf1d7b2b802166b7b83551d02a
Dinesh-Kumaar <dineshkumaar0412@gmail.com>
2026-03-14 14:48:23 +0530

    Caption icon functionality added

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,8 +3,10 @@ const TitleBox  = ({ message }) =>{
     return(
         <>
             <div className="h-24 flex flex-col m-1 bg-amber-900  p-4">
+                <h1 className="font-bold">Hari</h1>
                 <p >{message}</p>
             </div>
+            
         </>
     )
 }
--- src/pages/MeetingPage.jsx
+++ src/pages/MeetingPage.jsx
@@ -7,12 +7,12 @@ import { Navigate, useParams } from "react-router"
 import { goto } from "../assets/utils"
 //import {changeSubtitle} from "../components/ChatBox"
 const MeetingPage = () => {
+    const [isOpenCaption, setIsOpenCaption] = useState(false);
+    const cameraRef = useRef([]);
+    let { id } = useParams();
 
-    const cameraRef = useRef([])
-    let { id } = useParams()
-
-    let participantCount = parseInt(localStorage.getItem(id))
-    const [message, setMessage] = useState("Processing...");
+    let participantCount = parseInt(localStorage.getItem(id));
+    const [message, setMessage] = useState("Participant");
 
     const content = {
         1: "Hello",
@@ -37,18 +37,21 @@ const MeetingPage = () => {
                     <div className="navbar-center">
                         <Meet inField={<VideoIcon/>} color="primary" onClick={() => cameraRef.current.toggleCamera()} />
                         <Meet inField={<MicIcon />} color="accent-content" />
-                        <Meet inField={<ClosedCaptionIcon />} color="accent-content" />
+                        <Meet inField={<ClosedCaptionIcon />} color="accent-content" onClick={() => setIsOpenCaption(isOpenCaption => !isOpenCaption)} />
                         <Meet inField={<PhoneIcon />} color="accent-content" onClick={() => goto('/')} />
                         <Meet inField={1} color="accent-content" onClick={() => changeSubtitle(1)} />
                     </div>
                 </div>
 
-                <div className=" h-120 flex items-center gap-2 p-4 mb-4  shadow-lg rounded-2xl bg-base-100">
+                <div className=" h-auto flex-1 items-center gap-2 p-4 mb-4  shadow-lg rounded-2xl bg-base-100">
                     {[...Array(participantCount)].map((_, i) => <Camerae key={i} ref={(ref) => cameraRef.current[i] = ref} />)}
                 </div>
 
-                <div className="h-24 flex flex-row items-center gap-2 p-2 shadow-lg  rounded-2xl bg-base-100">
-                    <div className="h-24 flex flex-col-reverse justify-center p-4 basis-1/3 bg-amber-200">
+
+                {/* Caption open when openCaption Icon is clicked */}
+                {isOpenCaption ?  <div className="h-auto flex flex-row items-center gap-2 p-2 shadow-lg  rounded-2xl bg-base-100">
+                    <div className="h-auto flex flex-col-reverse justify-center p-4 basis-1/3 bg-amber-200">
+                        
                         <ChatBox  message={message} />
                     </div>
                     <div className="flex flex-row p-4 basis-2/3">
@@ -59,8 +62,7 @@ const MeetingPage = () => {
                          <Meet inField={4} color="accent-content" onClick={() => changeSubtitle(4)} />
                          <Meet inField={5} color="accent-content" onClick={() => changeSubtitle(5)} />
                     </div>
-                </div>
-
+                </div> : null}
             </div>
         </>
     )
