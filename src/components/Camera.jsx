c72ecb24141d0ff5a809cd8fb0d06a9bcf6c2bc8
KaniCanCode <nitheeshkani@gmail.com>
2026-03-14 15:56:15 +0530

    fixed the alignment issue

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, forwardRef, useImperativeHandle } from "react";
+import { useEffect, useRef, forwardRef, useImperativeHandle } from "react"
 
 const Camerae = forwardRef((props, ref) => {
-  const videoRef = useRef(null);
-  let streamRef = useRef(null);
+  const videoRef = useRef(null)
+  let streamRef = useRef(null)
 
   useEffect(() => {
     async function startCamera() {
@@ -10,44 +10,44 @@ const Camerae = forwardRef((props, ref) => {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: true,
           audio: false,
-        });
+        })
 
-        streamRef.current = stream;
-        videoRef.current.srcObject = stream;
+        streamRef.current = stream
+        videoRef.current.srcObject = stream
       } catch (error) {
-        alert("Please allow camera access");
+        alert("Please allow camera access")
       }
     }
 
-    startCamera();
+    startCamera()
 
     return () => {
       if (streamRef.current) {
-        streamRef.current.getTracks().forEach(track => track.stop());
+        streamRef.current.getTracks().forEach(track => track.stop())
       }
-    };
-  }, []);
+    }
+  }, [])
 
   const toggleCamera = () => {
-    const videoTrack = streamRef.current?.getVideoTracks()[0];
+    const videoTrack = streamRef.current?.getVideoTracks()[0]
     if (videoTrack) {
-      videoTrack.enabled = !videoTrack.enabled;
+      videoTrack.enabled = !videoTrack.enabled
     }
-  };
+  }
 
   // 🔥 Expose function to parent
   useImperativeHandle(ref, () => ({
     toggleCamera,
-  }));
+  }))
 
   return (
     <video
       ref={videoRef}
       autoPlay
       playsInline
-      className="rounded-4xl p-4"
+      className={"rounded-4xl m-2 " + props.height}
     />
-  );
-});
+  )
+})
 
-export default Camerae;
+export default Camerae
--- src/pages/MeetingPage.jsx
+++ src/pages/MeetingPage.jsx
@@ -7,12 +7,12 @@ import { Navigate, useParams } from "react-router"
 import { goto } from "../assets/utils"
 //import {changeSubtitle} from "../components/ChatBox"
 const MeetingPage = () => {
-    const [isOpenCaption, setIsOpenCaption] = useState(false);
-    const cameraRef = useRef([]);
-    let { id } = useParams();
+    const [isOpenCaption, setIsOpenCaption] = useState(false)
+    const cameraRef = useRef([])
+    let { id } = useParams()
 
-    let participantCount = parseInt(localStorage.getItem(id));
-    const [message, setMessage] = useState("Participant");
+    let participantCount = parseInt(localStorage.getItem(id))
+    const [message, setMessage] = useState("Participant")
 
     const content = {
         1: "Hello",
@@ -20,20 +20,20 @@ const MeetingPage = () => {
         3: "welcome",
         4: "victory",
         5: "Subtitle content for Button 5"
-    };
+    }
 
     const changeSubtitle = (num) => {
-        setMessage("Processing..."); // show loading text first
+        setMessage("Processing...") // show loading text first
 
         setTimeout(() => {
-            setMessage(content[num]);
-        }, Math.floor(Math.random() * (6000 - 3000 + 1)) + 3000);
-    };
+            setMessage(content[num])
+        }, Math.floor(Math.random() * (6000 - 3000 + 1)) + 3000)
+    }
     return (
         <>
             <div className="bg-base-200 p-4 h-screen flex flex-col overflow-hidden">
 
-                <div className="navbar h-1 max-w-full flex items-center justify-center bg-base-100 shadow-xl mb-5 rounded-2xl   ">
+                <div className="navbar max-w-full max-h-[20vh] flex items-center justify-center bg-base-100 shadow-xl mb-5 rounded-2xl   ">
                     <div className="navbar-center">
                         <Meet inField={<VideoIcon />} color="primary" onClick={() => cameraRef.current.toggleCamera()} />
                         <Meet inField={<MicIcon />} color="accent-content" />
@@ -43,31 +43,26 @@ const MeetingPage = () => {
                     </div>
                 </div>
 
-                <div className=" h-screen gap-4 flex-1 items-center p-4  shadow-lg rounded-2xl bg-base-100">
-                    <div className="flex flex-1 flex-wrap items-center justify-center gap-2 overflow-hidden">
-                        {[...Array(participantCount)].map((_, i) => (
-                            <div key={i} className="flex-1 min-w-[200px] max-h-full">
-                                <Camerae ref={(ref) => cameraRef.current[i] = ref} />
-                            </div>
-                        ))}
-                    </div>
-                    {/* {[...Array(participantCount)].map((_, i) => <Camerae key={i} ref={(ref) => cameraRef.current[i] = ref} />)} */}
-
-                    {/* Caption open when openCaption Icon is clicked */}
-                    {isOpenCaption ? <div className="h-auto flex flex-row items-center gap-2 p-2 shadow-lg rounded-2xl bg-base-100 transition-all">
-                        <div className="h-auto flex flex-col-reverse justify-center p-4 basis-1/3 bg-amber-200">
-                            <ChatBox message={message} />
-                        </div>
-                        <div className="flex flex-row p-4 basis-2/3">
-                            {/* Meeting Info  */}
-                            <Meet inField={1} color="accent-content" onClick={() => changeSubtitle(1)} />
-                            <Meet inField={2} color="accent-content" onClick={() => changeSubtitle(2)} />
-                            <Meet inField={3} color="accent-content" onClick={() => changeSubtitle(3)} />
-                            <Meet inField={4} color="accent-content" onClick={() => changeSubtitle(4)} />
-                            <Meet inField={5} color="accent-content" onClick={() => changeSubtitle(5)} />
-                        </div>
-                    </div> : null}
+                <div className={"gap-2 flex flex-wrap items-center justify-center p-2  shadow-lg rounded-2xl bg-base-100 " + (isOpenCaption ? "h-[60vh]" : "h-[80vh]")}>
+                    {[...Array(participantCount)].map((_, i) => (
+                        <Camerae ref={(ref) => cameraRef.current[i] = ref} height={(isOpenCaption ? "h-[55vh]" : "h-[72vh]")} />
+                    ))}
                 </div>
+
+                {/* Caption open when openCaption Icon is clicked */}
+                {isOpenCaption && <div className="h-auto flex flex-row items-center gap-2 p-2 shadow-lg rounded-2xl transition-all m-2">
+                    <div className="h-auto flex flex-col-reverse justify-center p-4 basis-1/3 bg-amber-200">
+                        <ChatBox message={message} />
+                    </div>
+                    <div className="flex flex-row p-4 basis-2/3">
+                        {/* Meeting Info  */}
+                        <Meet inField={1} color="accent-content" onClick={() => changeSubtitle(1)} />
+                        <Meet inField={2} color="accent-content" onClick={() => changeSubtitle(2)} />
+                        <Meet inField={3} color="accent-content" onClick={() => changeSubtitle(3)} />
+                        <Meet inField={4} color="accent-content" onClick={() => changeSubtitle(4)} />
+                        <Meet inField={5} color="accent-content" onClick={() => changeSubtitle(5)} />
+                    </div>
+                </div>}
 
 
 
